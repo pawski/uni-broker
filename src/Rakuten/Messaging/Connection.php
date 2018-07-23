ccbe6d46dfd35d0964169dee5280209934d42e6a
Pawel Czyzewski <pawel.czyzewski@enginewerk.com>
2018-07-23 21:33:29 +0200

    Init

---
+++ src/Rakuten/Messaging/Connection.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Rakuten\Messaging;
+
+class Connection
+{
+    public function open(): void
+    {
+
+    }
+
+    public function isOpen(): bool
+    {
+
+    }
+}
